637eb2bcf2e6afc3ba993a2d0c53edd02283bad0
signm4 <signm3@gmail.com>
2022-11-27 17:11:51 -0600

    fixed Encryption, tested and saw it is working, changed to AES 128bits

--- messageMaker.py
+++ messageMaker.py
@@ -1,9 +1,10 @@
 import random
-import base64
+from base64 import b64decode, b64encode
+import hashlib
 from Crypto.Cipher import AES
 from Crypto import Random
-from Crypto.Protocol.KDF import PBKDF2
-#please install venv, pycrypto "pip3 install pycrypto",
+# from Crypto.Random import get_random_bytes
+#please install venv, pycrypto "pip3 install pycryptodome",
 
 
 '''in this case we used pre made messages a Car would communicate, this can be always expanded using machine learning'''
@@ -13,67 +14,65 @@ def messageMaker():
     # print (chosenMessage) # uncomment to test to see if it is working
     return (chosenMessage)
 
+# https://medium.com/quick-code/aes-implementation-in-python-a82f582f51c2
 
-# AES 256 encryption/decryption using pycrypto library
-#https://www.quickprogrammingtips.com/python/aes-256-encryption-and-decryption-in-python.html
+#The class will recieve a key of any length then it will generate a 256bit has from that key.
+class AESCipher(object):
+    def __init__(self, key):
+        self.block_size = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
 
- 
-BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
- 
-# password = input("Enter encryption password: ") pushed to main()
- 
- #encrypting password using PBKDF2
-
-def get_private_key(password):
-    salt = b"this is a salt"
-    kdf = PBKDF2(password, salt, 64, 1000)
-    key = kdf[:32]
-    return key
- 
- 
-def encrypt(raw, password):
-    private_key = get_private_key(password)
-    raw = pad(raw)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
- 
- 
-def decrypt(enc, password):
-    private_key = get_private_key(password)
-    enc = base64.b64decode(enc)
-    iv = enc[:16]
-    cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
- 
- 
-# First let us encrypt secret message
-encrypted = encrypt("This is a secret message", password)
-print(encrypted)
- 
-# Let us decrypt using our original password
-decrypted = decrypt(encrypted, password)
-print(bytes.decode(decrypted))
+    #  Encrypt method recieves the plaintext and then pads it to add the extra bits it needs. then we generate a random "iv" with 128bits
+    #   using the PyCryptodome library we create the AES cipher by using AES.new("ourkey", CBC Mode, and our "iv") ... Explain CBC MODE
+    #we then use cipher.encrypt with our new AES system and decode it back from bits to readable characters
+    def encrypt(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8")
 
 
-'''using diffie hellman method for a Tangle CV, this will verify the number of the Genesis'''
-# def verifier():
-#     random.seed()
-#     #chooses a random number to use for the private key for "a", used 0 to 100 just as an example, for better security choosing a larger range would be better
-#     randomPrivKeyA = random.randint(0,100) 
-#     #chooses a random number to use for the private key for "b",
-#     randomPrivKeyB = random.randint(0,100)
+    # Decrypt is basically undoin everything encrypt() has done, first go back to bits then take out the iv, which is the 128 bits from encrypted_text
+    # a new AES key is generated in CBC mode and the "iv" we have taken out. last but not least we have to go form bits to text and take out the extra 
+    # characters by the unpad function. 
 
-#     #import function from lab5.py
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
+
+    #The Pad's job is to add extra characters to make it a multiple of 128, since we are using AES with 128bits
+    def __pad(self, plain_text):
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        return padded_plain_text
+
+    # UnPad's job is to recive the plaintext and take out the extra characters
+    @staticmethod
+    def __unpad(plain_text):
+        last_character = plain_text[len(plain_text) - 1:]
+        return plain_text[:-ord(last_character)]
+
+
+
 
 
 def main():
     newMessage = messageMaker() # a message has been made 
-    password = input("Enter encryption password: ") # key to which our encryption will be done with
-    encryptedMsg = encrypt(newMessage, password)
-    print(encryptedMsg)
-
+    key = input("Enter a Key Phrase: ")
+    a = AESCipher(key)
+    encryptedMsg = a.encrypt(newMessage)
+    print("\n\nthis is the encrypted message: ", encryptedMsg)
+    
+    print("\nNow we will decrypt the message")
+    DKey = input("Enter the key used before: ")
+    b = AESCipher(DKey)
+    decryptedMsg = b.decrypt(encryptedMsg)
+    print("\n\nHere is the Decrypted Message: ", decryptedMsg)
 if __name__ == '__main__': 
     main() 
